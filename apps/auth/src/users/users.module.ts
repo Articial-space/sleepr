97a9ae7389d9adb0d8510d9dc97c8e1d3c19f1c2
KhoaMinh <kevinkhoa0203@gmail.com>
2024-12-06 08:16:56 +0000

    Passport

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DatabaseModule } from '@app/common';
+import { UsersDocument, UsersSchema } from './models/users.schema';
+import { LoggerModule } from 'nestjs-pino';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [DatabaseModule, DatabaseModule.forFeature([{name: UsersDocument.name, schema: UsersSchema}]), LoggerModule.forRoot(
+    {
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLines: true
+          }
+        }
+      }
+    }
+  )],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository]
+})
+export class UsersModule {}
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -5,6 +5,8 @@ import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [DatabaseModule, DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}]), LoggerModule.forRoot(
@@ -18,7 +20,15 @@ import { LoggerModule } from 'nestjs-pino';
         }
       }
     }
-  )],
+  ),
+  ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      MONGODB_URI: Joi.string().required(),
+      PORT: Joi.number().required()
+    })
+  })
+],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
